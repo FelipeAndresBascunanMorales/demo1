1cc2e77c5c678c17176ed09341f1490ad4c69e81
fbascunan <fbascunan@buk.cl>
2022-06-09 19:59:28 -0400

    feat: super cool feature

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hola gola <a href="https://nextjs.org">Next.js!</a>
+          Hola integraciones <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
